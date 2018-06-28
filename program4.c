24ee796740232d54c6d5ba753c48fd7d2975f415
PeriyanayakiR <34892736+PeriyanayakiR@users.noreply.github.com>
2018-06-28 22:46:15 +0530

    Create program4.c

---
+++ program4.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{
+    char ch;
+    scanf("%c",&ch);
+    if((ch>='A')&&(ch<='z')||(ch>='a')&&(ch<='z'))
+    {
+     printf("Alphabet");
+    }
+    else
+    {
+        printf("No");
+    }
+   return 0;
+}
